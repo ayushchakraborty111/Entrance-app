c5dab7cc605bde67ebdf86b9457badde9eca4d93
ayushchakraborty111 <ayushchakraborty.cse19@chitkarauniversity.edu.in>
2021-10-06 16:36:10 +0530

    Initial commit

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+// const hour = document.querySelector('#cinh');
+// const min = document.querySelector('#cinm');
+
+// hour.value = Number(new Date().getHours());
+// min.value = Number(new Date().getMinutes());
+
+// hour.disabled = true;
+// min.disabled = true;
